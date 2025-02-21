587d998c4702bb8e5b83f5c01bdd0f6e544b0d9c
roman khan <romankhan.26@outlook.com>
2025-02-21 20:42:49 +0500

    Initial commit from Create Next App

---
+++ src/Components/Generals/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h3 className='text-2xl font-bold font-serif p-5  bg-green-900 text-yellow-500'>
+        CarRental
+        </h3>   </div>
+  )
+}
+
+export default Navbar
